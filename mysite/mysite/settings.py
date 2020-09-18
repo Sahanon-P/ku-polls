c3b3f510f8007ba3fecc12d00f989dc095f75669
Sahanon Phisetpakasit <sahanon.p@ku.th>
2020-09-18 23:12:05 +0700

    externalize configuration data

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,19 +11,18 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')#5zw=gttx(q#y1lze)3^141+$=7)_3_i0fd+6_^v(bn5(bd4='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default= False, cast=bool)
 
 ALLOWED_HOSTS = []
 
