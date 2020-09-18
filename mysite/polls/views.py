4efcafe64674d50872149108f5b04317e5e8d6ab
Sahanon Phisetpakasit <sahanon.p@ku.th>
2020-09-18 21:52:50 +0700

    add redirect when it cause an error

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,35 +4,9 @@ from django.http import HttpResponseRedirect
 from django.views import generic
 from django.utils import timezone
 from django.urls import reverse
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Question, Choice
-
-
-# from django.template import loader
-# Create your views here.
-
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by
-#     # output = ', '.join([q.question_text for q in latest_question_list])
-#     # template = loader.get_template('polls/index.html')
-#     context = {'lastest_question_list': latest_question_list, }
-#     return render(request, 'polls/index.html', context)
-#     # return HttpResponse(template.render(context,request))
-
-
-# def detail(request, question_id):
-#     # try:
-#     #     question = Question.objects.get(pk=question_id)
-#     # except Question.DoesNotExist:
-#     #     raise Http404("Question does nit exist")
-#     # return HttpResponse("You're looking at question %s" % question_id)
-#     question = get_object_or_404(Question,pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
-
-
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk = question_id)
-#     return render(request, 'polls/results.html',{'question':question})
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -45,6 +19,16 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get(self, request, **kwargs):
+        try:
+            question = Question.objects.get(pk=kwargs['pk'])
+            if not question.can_vote():
+                return HttpResponseRedirect(reverse('polls:index'), messages.error(request, "This poll is already closed. Can't vote!!!"))
+        except ObjectDoesNotExist:
+            return HttpResponseRedirect(reverse('polls:index'), messages.error(request, "This poll is not exist."))
+        self.object = self.get_object()
+        return self.render_to_response(self.get_context_data(object=self.get_object()))
+
     def get_queryset(self):
         return Question.objects.filter(pub_date__lte=timezone.now())
 
@@ -57,7 +41,7 @@ class ResultsView(generic.DetailView):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if not question.can_vote():
-        return HttpResponseRedirect(reverse("polls:index"))
+        return HttpResponseRedirect(reverse('polls:index'), messages.error("Can't vote after poll closed."))
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
